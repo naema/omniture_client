c8fb0269cce78bdd9ae2c7bca71f33b981930114
naema <naema@lavabit.com>
2013-03-19 11:08:19 +0100

    build gem

---
+++ lib/omniture_client/version.rb
@@ -0,0 +1,3 @@
+module OmnitureClient
+  VERSION = "0.1.4"
+end
---
+++ lib/railtie.rb
@@ -0,0 +1,9 @@
+# module OmnitureClient
+#   class Railtie < Rails::Railtie
+#     initializer "" do
+#       ActiveSupport.on_load(:action_controller) do
+#         include OmnitureClient
+#       end
+#     end
+#   end
+# end
